4b799b27127bb0e8c7f27d2455765ec07352f58b
Ivan <githubh@gmail.com>
2016-07-31 20:18:51 +0300

    Add ability mark Task as completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728193038) do
+ActiveRecord::Schema.define(version: 20160731165134) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20160728193038) do
   create_table "tasks", force: :cascade do |t|
     t.string   "content"
     t.integer  "project_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.datetime "completed_at"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
